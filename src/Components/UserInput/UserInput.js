bad8d56566063b1be404615b591a3df0e1f271f1
Yash1Hingu <yash23hingu@gmail.com>
2023-05-22 18:06:28 +0530

    UPDATE -- useRef hook instend of useState

--- src/Components/UserInput/UserInput.js
+++ src/Components/UserInput/UserInput.js
@@ -1,51 +1,57 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { createPortal } from 'react-dom';
 import styled from './UserInput.module.css'
 import ErrorDialog from "../ErrorDialog/ErrorDialog";
 const UserInput = (props) => {
-    const [EnterName, setName] = useState('');
-    const [EnterAge, setAge] = useState('');
+    const enterName = useRef();
+    const enterAge = useRef();
     const [message, setMessage] = useState('');
     const [open, setOpen] = useState(false);
 
-    const handlerEnterName = (event) => {
-        setName(event.target.value);
-    }
-    const handlerEnterAge = (event) => {
-        setAge(Number(event.target.value));
-    }
     const handlerDialog = (event) => {
         event.preventDefault()
         setOpen(false);
-        setAge('');
+        enterAge.current.value = '';
     }
     const handlerSubmit = (event) => {
         event.preventDefault();
-        if (EnterName === '' && EnterAge === '') {
+        const enterUserName = enterName.current.value;
+        const enterUserAge = enterAge.current.value;
+        if (enterUserName === '' && enterUserAge === '') {
             setOpen(true);
             setMessage("Please Enter Username & Age.");
             return;
-        } else if (EnterAge <= 1) {
+        } else if (enterUserAge <= 1) {
             setOpen(true);
             setMessage("Enter Age > 0.");
             return;
         }
         const userData = {
             id: Math.random().toString(),
-            name: EnterName,
-            age: EnterAge
+            name: enterUserName,
+            age: enterUserAge
         }
-        setName('');
-        setAge('');
+        enterName.current.value = ''
+        enterAge.current.value = ''
         props.onSubmit(userData);
     }
     return (
         <div className={styled.user_input}>
             <form onSubmit={handlerSubmit} action="/">
                 <label htmlFor="username">User Name</label>
-                <input type="text" name="username" id="username" value={EnterName} onChange={handlerEnterName} />
+                <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    ref={enterName}
+                />
                 <label htmlFor="userage">Age</label>
-                <input type="number" name="userage" id="userage" value={EnterAge} onChange={handlerEnterAge} />
+                <input
+                    type="number"
+                    name="userage"
+                    id="userage"
+                    ref={enterAge}
+                />
                 <button type="submit">Submit</button>
             </form>
             {createPortal(
